475bdac85c915ddeb8866959b0a1ae98e28d3cfe
Ashish-05 <ashish@trendonix.com>
2017-11-19 17:24:24 +0530

    5:23 pm 19/11/2017
    Temperature and Humidity Sensor Code

---
+++ Temp+Humidity_cutoff.ino
@@ -0,0 +1,28 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  if(DHT.temperature>50)
+  {
+     Serial.println("hot hot hot");
+  }
+  else
+  {
+     Serial.println("heh!");
+  }
+  delay(1000);
+}
+
